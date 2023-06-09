8cf3ac691fe58a972098ff6e67c737ccc7d4d5cc
ScmTble <ScmTble@qq.com>
2023-06-09 23:32:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import axios from 'axios';
+
+
+const handleSetCookie = () => {
+  axios.get("https://nginx-proxy-st.zeabur.app/api/setcookie")
+}
+
+const handleGetCookie = () => {
+  axios.get("https://nginx-proxy-st.zeabur.app/api/getcookie", {
+    withCredentials: true
+  }).then(res => {
+    console.log(res)
+  })
+}
+
+</script>
+
+
+<template>
+  <div>
+    <button @click="handleSetCookie">设置Cookie</button>
+    <button @click="handleGetCookie">获取Cookie</button>
+  </div>
+</template>
+
