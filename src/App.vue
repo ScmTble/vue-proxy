892920929cc9082a670653bbfdb42b8f40347ec9
ScmTble <ScmTble@qq.com>
2023-06-10 00:32:44 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 
 const handleSetCookie = () => {
-  axios.get("https://nginx-proxy-st.zeabur.app/api/setcookie")
+  axios.get("https://nginx-proxy-st.zeabur.app/api/setcookie", {
+    withCredentials: true
+  })
 }
 
 const handleGetCookie = () => {
